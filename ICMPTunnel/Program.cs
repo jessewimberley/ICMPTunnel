0271faa3ef5025520040661a5e5bfd3cfeeb2eae
Jesse Wimberley <jesse.wimberley@gmail.com>
2020-12-14 09:44:27 -0800

    Add project files.

---
+++ ICMPTunnel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace ICMPTunnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte[] message = Encoding.ASCII.GetBytes("ICMP Tunnel Exfiltration");
+            Ping packet = new Ping();
+            packet.Send("8.8.8.8", 32, message);
+            // Use wireshark to see the message in the packet.
+        }
+    }
+}
