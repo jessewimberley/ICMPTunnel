b34027542f32a5ade41832602fdc29f7475e31f9
Jesse Wimberley <jesse.wimberley@gmail.com>
2020-12-14 10:04:57 -0800

    Added  comments

--- ICMPTunnel/Program.cs
+++ ICMPTunnel/Program.cs
@@ -10,6 +10,7 @@ namespace ICMPTunnel
         {
             Byte[] message = Encoding.ASCII.GetBytes("ICMP Tunnel Exfiltration");
             Ping packet = new Ping();
+            // Parameters are IP Address (string), TTL (int) and the message (byte array).
             packet.Send("8.8.8.8", 32, message);
             // Use wireshark to see the message in the packet.
         }
